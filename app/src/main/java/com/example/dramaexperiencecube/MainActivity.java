ea0c6afbae3a6a17abc28064834b28de5b6c632f
DaeSu <jews333333@gmail.com>
2019-07-10 15:03:56 +0900

    첫 화면 권한 요청

--- app/src/main/java/com/example/dramaexperiencecube/MainActivity.java
+++ app/src/main/java/com/example/dramaexperiencecube/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dramaexperiencecube;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -11,11 +15,20 @@ public class MainActivity extends AppCompatActivity {
 //Main화면, 시작화면
     Button btn_sub_activity1,btn_sub_activity2;
     ImageButton btn_help;
+    static final int PERMISSON=1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //권한이 부여되어 있는지 확인
+        getAll_permissions();
+
+
+
 
         btn_sub_activity1 = (Button)findViewById(R.id.btn_subActivity1);
         btn_sub_activity2 = (Button)findViewById(R.id.btn_subActivity2);
@@ -45,4 +58,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public Boolean getAll_permissions(){
+
+        //권한이 부여되어 있는지 확인
+        int permissonRecord= ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        int permissonWrite= ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissonRead= ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if(permissonRecord == PackageManager.PERMISSION_GRANTED && permissonWrite == PackageManager.PERMISSION_GRANTED  && permissonRead == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)){
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO}, PERMISSON);
+            }else{
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO}, PERMISSON);
+            }
+        }
+        return true;
+    }
+
 }
